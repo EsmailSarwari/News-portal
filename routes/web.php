1588926b8f9137e466f316a7bbb2ff68c06a75dc
Esmail Sarwari <esmail.sarwari10@gmail.com>
2023-05-13 22:33:40 +0300

    breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
@@ -17,6 +18,18 @@ use App\Http\Controllers;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::prefix('members')->group(function (){
     Route::get('register', [Controllers\membersController::class, 'register'] );
@@ -43,15 +56,5 @@ Route::prefix('cache')->group(function (){
 Route::prefix('mail')->group(function(){
     Route::get('/shipment', [Controllers\mailController::class, 'send']);
 });
-
-
-
-
-
-
-
-
-
-
 
 
